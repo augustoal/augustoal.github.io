007b6a7b8be978967f92bcecdbf9d1b1af2e5a90
Augusto Ariztia <augusto@MacBook-Pro-de-Augusto.local>
2017-05-16 16:16:36 -0400

    pruebas con el retorno

--- Barcode/barcode.py
+++ Barcode/barcode.py
@@ -66,8 +66,16 @@ def main():
 	session.release()
 	output.release()
 	if found_codes:
-		print('All scanned codes:\n' + '\n'.join(found_codes))
+		return found_codes
+	else:
+		return None
+		#print('All scanned codes:\n' + '\n'.join(found_codes))
 
 if __name__ == '__main__':
-	main()
+	codigos = main()
+	if not codigos:
+		print("Tipo de archivo: {}".format(type(codigos)))
+		print("Que códigos quieres guardar?")
+		for i,j in enumerate(codigos):
+			print("{}: {}".format(i,j))
 
